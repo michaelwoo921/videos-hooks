20ba23f4878e2f8258ba1085374271ec1ca4e30d
Michael <michaelwoo921@gmail.com>
2020-07-24 23:38:45 -0700

    v2

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ import useVideos from '../hooks/useVideos';
 
 const App = () => {
     const [selectedVideo,  setSelectedVideo] = useState(null);
-    const [videos, search] = useVideos('prince');
+    const [videos, search] = useVideos('javascript');
 
     useEffect(() => {
         setSelectedVideo(videos[0])
